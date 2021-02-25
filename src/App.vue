22cbe832c916ee9515cb90252d75cd04727b2133
BrianBell422 <brianbell.uk@gmail.com>
2021-02-25 11:47:32 +0000

    Selected beer info showing in browser

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div id="app">
     <h1>Beers</h1>
     <div class="main-container">
-
-
+      <beer-list :beers="beers"></beer-list>
+      <beer-detail :beer="selectedBeer"></beer-detail>
     </div>
   </div>
 </template>
 
 <script>
+import BeerList from './components/BeerList.vue'
+import BeerDetail from './components/BeerDetail.vue'
 
+import { eventBus } from './main.js';
 
 export default {
   name: 'App',
@@ -20,13 +23,18 @@ export default {
     };
   },
   components: {
-    
+    "beer-list": BeerList,
+    "beer-detail": BeerDetail
   },
   mounted(){
     fetch('https://api.punkapi.com/v2/beers')
     .then(res => res.json())
     .then(beers => this.beers = beers)
-  }
+
+    eventBus.$on('beer-selected', (beer) => {
+      this.selectedBeer = beer
+    })
+  },
 }
 </script>
 
---
+++ src/components/BeerDetail.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <div v-if="beer" id="beerDetail">
+        <h3>{{beer.name}}</h3>
+        <div>
+            <p><span>Description</span>: {{beer.description}}</p>
+            <p><span>ABV</span>: {{beer.abv}}</p>
+            <p><span>Volume</span>: {{beer.volume.value}} {{beer.volume.unit}}</p>
+        </div>
+        <div>
+            <img :src="beer.image_url" :alt="beer.name">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'beer-detail',
+    props: ['beer']
+}
+</script>
+
+<style>
+
+</style>
